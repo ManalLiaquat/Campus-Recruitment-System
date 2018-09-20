8011dd84c3e635b9d9b67183a1c0167516d1262b
ManalLiaquat <m.manalliaquat@gmail.com>
2018-09-21 01:13:51 +0500

    adding student dashboard component to main dashboard

--- src/Screens/Dashboard/Dashboard.js
+++ src/Screens/Dashboard/Dashboard.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
+import StudentDash from "./SubDashboards/StudentDash";
+
 class Dashboard extends Component {
   constructor() {
     super();
     this.state = {};
   }
 
-  studentDashboard() {}
-
   render() {
     const authAs = localStorage.getItem("authAs");
     return (
       <div>
-        <li>dash</li>
-        <li>board</li>
+        <div className="container-fluid">
+          <StudentDash />
+          {/* <div className="row">
+            <div className="col-md-3">
+              
+            </div>
+            <div className="col-md-9">dashboard</div>
+          </div> */}
+        </div>
       </div>
     );
   }
 }
+export default Dashboard;
