a9cdb2b840f0054ffaecc720ac7bbc66947545ef
Manal Liaquat <m.manalliaquat@gmail.com>
2018-09-23 00:19:42 +0500

    Getting and rendering jobs posted by companies

--- src/Screens/Dashboard/SubDashboards/StudentDash.js
+++ src/Screens/Dashboard/SubDashboards/StudentDash.js
@@ -14,8 +14,11 @@ class StudentDash extends Component {
       qualification: "",
       percentage: null,
       experience: "",
+      // other states
       checkInfo: false,
-      resume: null
+      resume: null,
+      content: "myInfo",
+      jobs: []
     };
     this.submitMyInfo = this.submitMyInfo.bind(this);
   }
@@ -53,7 +56,8 @@ class StudentDash extends Component {
         address,
         qualification,
         percentage,
-        experience
+        experience,
+        uid: fire.auth().currentUser.uid
       };
       console.log(studentResume);
       fire
@@ -83,11 +87,27 @@ class StudentDash extends Component {
       });
   }
 
+  renderJobs() {
+    let { jobs } = this.state;
+    fire
+      .database()
+      .ref("/company_jobs")
+      .on("child_added", data => {
+        let job = data.val();
+        for (const key in job) {
+          console.log(job[key]);
+          jobs.push(job[key]);
+        }
+        this.setState({ jobs: jobs });
+      });
+  }
+
   mySavedInfo() {
     const { resume } = this.state;
     console.log(resume);
     return (
       <div>
+        <h1 className="text-uppercase text-primary">My Info</h1>
         <label>
           Name:
           <input
@@ -191,6 +211,7 @@ class StudentDash extends Component {
   myUnsavedInfo() {
     return (
       <div>
+        <h1 className="text-uppercase text-primary">My Info</h1>
         <label>
           Name:
           <input
@@ -308,10 +329,80 @@ class StudentDash extends Component {
 
   render() {
     const authAs = localStorage.getItem("authAs");
+    const { checkInfo, content, jobs } = this.state;
     return (
       <div>
         <div className="container-fluid">
           <div className="row">
+            <div className="col-md-9">
+              {/* <h1
+                style={{
+                  textTransform: "uppercase"
+                }}
+              >
+                Dashboard
+              </h1> */}
+              <br />
+              {content === "myInfo" ? (
+                checkInfo ? (
+                  this.mySavedInfo()
+                ) : (
+                  this.myUnsavedInfo()
+                )
+              ) : content === "myApplications" ? (
+                <h1 className="text-uppercase text-dark">My Applications</h1>
+              ) : content === "findAJob" ? (
+                <div>
+                  <h1 className="text-uppercase text-danger">Find A Job</h1>
+                  <div>
+                    {jobs.map((job, index) => {
+                      return (
+                        <div
+                          className="card text-white bg-dark mb-3"
+                          // style={{ maxWidth: "18rem" }}
+                        >
+                          <div className="card-header">
+                            <div className="row">
+                              <div className="col-11">{job.companyName}</div>
+                              <div className="col-1">
+                                <i className="fa fa-heart-o" />
+                              </div>
+                            </div>
+                          </div>
+                          <div className="card-body">
+                            <h5 className="card-title">{job.title}</h5>
+                            <p className="card-text">{job.description}</p>
+                            <div>Skills: {job.skills}</div>
+                            <div
+                              className="row text-warning"
+                              style={{ fontSize: "10pt" }}
+                            >
+                              <div className="col">
+                                Eligibility: {job.eligibility}
+                              </div>
+                              <div className="col">Salary: {job.salary}</div>
+                            </div>
+                          </div>
+                          <div className="card-footer">
+                            <div className="row">
+                              <div className="col-md-10">Time: {job.time}</div>
+                              <div className="col-md-2">
+                                <button className="btn btn-block btn-sm btn-primary">
+                                  Apply
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              ) : (
+                <br />
+              )}
+            </div>
+
             <div className="col-md-3">
               <ul className="navbar-nav">
                 <li className="nav-link">
@@ -319,33 +410,34 @@ class StudentDash extends Component {
                     className="btn btn-primary btn-block"
                     onClick={() => {
                       this.componentWillMount();
+                      this.setState({ content: "myInfo" });
                     }}
                   >
                     My Info
                   </button>
                 </li>
                 <li className="nav-link">
-                  <button className="btn btn-success btn-block">
+                  <button
+                    className="btn btn-success btn-block"
+                    onClick={() => {
+                      this.setState({ content: "myApplications" });
+                    }}
+                  >
                     My Applications
                   </button>
                 </li>
                 <li className="nav-link">
-                  <button className="btn btn-danger btn-block">
+                  <button
+                    className="btn btn-danger btn-block"
+                    onClick={() => {
+                      this.setState({ content: "findAJob" });
+                      this.renderJobs();
+                    }}
+                  >
                     Find a Job
                   </button>
                 </li>
               </ul>
-            </div>
-            <div className="col-md-9">
-              <h1
-                style={{
-                  textTransform: "uppercase"
-                }}
-              >
-                Dashboard
-              </h1>
-              <br />
-              {this.state.checkInfo ? this.mySavedInfo() : this.myUnsavedInfo()}
             </div>
           </div>
         </div>
