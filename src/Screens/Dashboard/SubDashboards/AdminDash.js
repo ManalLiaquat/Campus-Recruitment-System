b98ce038f8d11406fdd6c579d0c77374e61b540e
Manal Liaquat <m.manalliaquat@gmail.com>
2018-09-23 21:33:49 +0500

    just created!

---
+++ src/Screens/Dashboard/SubDashboards/AdminDash.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class AdminDash extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Admin Panel</h1>;
+  }
+}
+
+export default AdminDash;
