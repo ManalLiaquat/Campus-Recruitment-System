edffa3d2d0bd0bfdc050e732e26984fad77ab104
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-11 23:43:02 +0500

    getting students login details

--- src/Screens/Dashboard/SubDashboards/AdminDash.js
+++ src/Screens/Dashboard/SubDashboards/AdminDash.js
@@ -6,16 +6,22 @@ class AdminDash extends Component {
     super();
     this.state = {
       show: "student",
-      toggleView: false
+      stdLogin_data: [],
+      cmpLogin_date: []
     };
   }
 
   studentLogin_FUNC() {
+    const { stdLogin_data } = this.state;
     fire
       .database()
       .ref("/student_data")
       .once("value", data => {
-        console.log(data.val());
+        let obj = data.val();
+        for (const key in obj) {
+          stdLogin_data.push(obj[key]);
+        }
+        this.setState({ stdLogin_data });
       });
   }
 
@@ -30,13 +36,12 @@ class AdminDash extends Component {
   }
 
   render() {
-    const { show } = this.state;
+    const { show, stdLogin_data } = this.state;
 
     return (
       <div>
         <h3 className="text-info" style={{ marginTop: "10px" }}>
-          Welcome Admin! ({localStorage.getItem("displayName")}){" "}
-          <i className="fa fa-user-circle-o" />
+          Welcome Admin! ({localStorage.getItem("displayName")})
           <button
             // style={{padding:'10px', marginTop:"10px"}}
             className="btn btn-info pull-right"
@@ -53,47 +58,83 @@ class AdminDash extends Component {
         <br />
         {show === "student" && (
           <div className="row">
-            <div className="col-md-6">
-              <h3
-                className="text-center"
-                style={{
-                  margin: "10px",
-                  backgroundColor: "whitesmoke",
-                  minHeight: "75vh"
-                }}
-              >
-                Students login detail
-              </h3>
+            <div
+              className="col-md-6"
+              style={{
+                backgroundColor: "whitesmoke",
+                minHeight: "75vh"
+              }}
+            >
+              <h3 className="text-center">Students login detail</h3>
+              <table class="table table-sm table-hover table-striped table-responsive">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Password</th>
+                    <th scope="col">UID</th>
+                    <th scope="col">Inbox</th>
+                    <th scope="col">Manage</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {stdLogin_data.map((v, i) => {
+                    return (
+                      <tr key={v.uid + " " + i}>
+                        <th scope="row">{i + 1}</th>
+                        <td>{v.name}</td>
+                        <td>{v.email}</td>
+                        <td>{v.password}</td>
+                        <td>{v.uid}</td>
+                        <td>
+                          <button className="btn btn-info btn-block btn-sm">
+                            <i className="fa fa-info" />
+                          </button>
+                        </td>
+                        <td>
+                          <button className="btn btn-sm btn-success">
+                            <i className="fa fa-edit" />
+                          </button>
+                          <button className="btn btn-sm btn-danger">
+                            <i className="fa fa-trash-o" />
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
             </div>
-            <div className="col-md-6">
-              <h3
-                className="text-center"
-                style={{
-                  margin: "10px",
-                  backgroundColor: "whitesmoke",
-                  minHeight: "75vh"
-                }}
-              >
-                Students Job Data
-              </h3>
+            <div
+              className="col-md-6"
+              style={{
+                backgroundColor: "whitesmoke",
+                minHeight: "75vh"
+              }}
+            >
+              <h3 className="text-center">Students Job Data</h3>
             </div>
           </div>
         )}
         {show === "company" && (
           <div className="row">
-            <div className="col-md-6">
-              <h3
-                className="text-center"
-                style={{
-                  margin: "10px",
-                  backgroundColor: "whitesmoke",
-                  minHeight: "75vh"
-                }}
-              >
-                Companies login detail
-              </h3>
+            <div
+              className="col-md-6"
+              style={{
+                backgroundColor: "whitesmoke",
+                minHeight: "75vh"
+              }}
+            >
+              <h3 className="text-center">Companies login detail</h3>
             </div>
-            <div className="col-md-6">
+            <div
+              className="col-md-6"
+              style={{
+                backgroundColor: "whitesmoke",
+                minHeight: "75vh"
+              }}
+            >
               <h3
                 className="text-center"
                 style={{
